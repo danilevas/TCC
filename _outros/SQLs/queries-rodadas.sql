9942d9bbcc216cbe39e4dffa1ad30eb7c1d2c977
danilevas <dlevacov@gmail.com>
2025-07-26 23:30:58 -0300

    pequenas adições de SQL

--- _outros/SQLs/queries-rodadas.sql
+++ _outros/SQLs/queries-rodadas.sql
@@ -273,3 +273,48 @@ WHERE LENGTH(id_ufrj) NOT IN (11)
 
 -- Basicamente aqui temos 43 códigos de gringos que tem entre 8 e 10 caracteres, números e letras,
 -- todos começando com "E", e 1 CPF de um cara que só tem 10 dígitos
+
+-- Caronas com done=True sem nenhum pedido com status accepted e seus pedidos
+SELECT rides.id, rides.created_at, rides.updated_at, rides.date, ride_user.created_at, ride_user.updated_at,
+ride_user.status FROM rides
+LEFT JOIN ride_user ON rides.id = ride_user.ride_id
+WHERE rides.done = TRUE
+AND rides.id NOT IN (
+	SELECT DISTINCT rides.id FROM rides
+	JOIN ride_user ON rides.id = ride_user.ride_id
+	WHERE rides.done = TRUE
+	AND ride_user.status = 'accepted'
+)
+ORDER BY rides.id
+
+-- Dessas, 23 tem pedidos e 34 não.
+
+-- 5984 caronas com done=TRUE
+-- 5950 caronas com done=TRUE com algum pedido
+-- 5927 caronas com done=TRUE com algum pedido accepted
+
+-- Das 23 caronas com pedidos, 19 tem algum pedido quit, ou seja, tiveram algum pedido accepted em algum momento. 4 só tem pedido driver. Temos 7 pedidos quit ocorridos após a hora em que a carona deveria acontecer (em teoria).
+
+-- Podemos teorizar que essas 34 sem pedidos foram apagadas, e que essas 23 com pedidos
+
+-- Auxílio
+SELECT DISTINCT rides.id FROM rides
+JOIN ride_user ON rides.id = ride_user.ride_id
+WHERE rides.done = TRUE
+AND ride_user.status = 'accepted'
+ORDER BY rides.id
+
+-- Caronas com done=True sem nenhum pedido com status accepted e seus pedidos
+SELECT rides.id, rides.created_at, rides.updated_at, rides.date, ride_user.created_at, ride_user.updated_at,
+ride_user.status FROM rides
+JOIN ride_user ON rides.id = ride_user.ride_id
+WHERE rides.done = TRUE
+AND rides.id NOT IN (
+	SELECT DISTINCT rides.id FROM rides
+	JOIN ride_user ON rides.id = ride_user.ride_id
+	WHERE rides.done = TRUE
+	AND ride_user.status = 'accepted'
+)
+-- AND ride_user.status = 'quit'
+-- AND ride_user.updated_at > rides.date
+ORDER BY rides.created_at, ride_user.created_at
